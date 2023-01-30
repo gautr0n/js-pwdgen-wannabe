8066f0439237338f26e85d0f88c1307f50eb1a38
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-01-30 15:51:37 +0100

    aggiunta restanti richieste

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 let firstName = prompt('inserisci il tuo nome');
 console.log(firstName);
+
+let secondName = prompt('inserisci il tuo cognome');
+console.log(secondName);
+
+let favColor = prompt('inserisci il tuo colore preferito');
+console.log(favColor);
+
+let favNumber = prompt('inserisci il tuo numero preferito');
+console.log(favNumber);
+
+let result = firstName + secondName + favColor + favNumber;
+console.log(result);
